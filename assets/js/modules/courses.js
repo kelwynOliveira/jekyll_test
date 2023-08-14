a533e4f404b20c427f5e1930d49b5ac4480443b7
Kelwyn Oliveira <kelwyn.oliveira@gmail.com>
2023-08-13 21:10:07 -0400

    Theme updates

---
+++ assets/js/modules/courses.js
@@ -0,0 +1,25 @@
+export default function initFetchCourses() {
+  async function fetchCourses(url) {
+    try {
+      const coursesResponse = await fetch(url);
+      const coursesJSON = await coursesResponse.json();
+      const coursesList = document.querySelector("[data-list='projects']");
+
+      coursesJSON.forEach((course) => {
+        coursesList.innerHTML += `
+        <li>
+        <a href="${course.url}" target="_blank">
+        <img class="thumb" src="${course.thumb}" alt="${course.project_name}">
+        <p>Project name: <span>${course.project_name}</span></p>
+        <p>Technologies: <span>${course.technolgies}</span></p>
+        </a>
+        </li>  
+        `;
+      });
+    } catch (erro) {
+      console.log(erro);
+    }
+  }
+
+  fetchCourses("/assets/data/coursesProjectsapi.json");
+}
---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+import initFetchCourses from "./modules/courses.js";
+import initSocials from "./modules/socials.js";
+
+initFetchCourses();
+initSocials();
